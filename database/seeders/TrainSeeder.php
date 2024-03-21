c9013db16ff85d2971657d4d79a3e783670d223b
DemetrioPadre <demetriopadre@gmail.com>
2024-03-21 21:28:28 +0100

    add seed

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -16,5 +16,18 @@ class TrainSeeder extends Seeder
     public function run()
     {
         $train = new Train;
+
+
+        $train->azienda = "LaravelTrain";
+        $train->stazione_di_partenza = "Roma";
+        $train->stazione_di_arrivo = "Paradiso";
+        $train->codice_treno = "1234";
+        $train->numero_carrozze = 2;
+        // $train->in_orario = 1;
+        // $train->cancellato = 0;
+        $train->orario_di_partenza = "2024-03-21 08:00:00";
+        $train->orario_di_arrivo = "2024-03-21 17:00:00";
+
+        $train->save();
     }
 }
